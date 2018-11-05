611a237f97cf895d1aafb24bc49d7ede063b7860
leancoggiola <leandrocoggiola@hotmail.com>
2018-11-05 19:04:54 -0300

    Gauge semana terminado

--- src/app/_views/evaluacion-view/evaluacion-view.component.ts
+++ src/app/_views/evaluacion-view/evaluacion-view.component.ts
@@ -1,7 +1,6 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {EvaluacionService} from '../../_services/evaluacion.service';
 import {MAT_DIALOG_DATA, MatDialog, MatDialogRef, MatSnackBar} from '@angular/material';
-import {VerMenuConfirmRemComponent} from '../ver-menu-view/ver-menu-view.component';
 
 @Component({
   selector: 'app-evaluacion-view',
@@ -13,74 +12,144 @@ export class EvaluacionViewComponent implements OnInit {
 
   mostrarSemana = true;
   diaEvaluacion = 'Semana';
+  ValoresSemana;
+  ValoresDia;
+  ValoresSelected;
+  CaloriasRecomendada;
+  CaloriasRequerida;
+  cargaDatos = false;
 
-  public radarChartLabels: string[] = ['Eating', 'Drinking', 'Sleeping', 'Designing', 'Coding', 'Cycling', 'Running'];
+  public InfoCalorias = {
+    // Configuracion
+    chart: {
+      // Titulo
+      caption: 'CALORIAS CONSUMIDAS',
+      captionFontSize: 20,
+      captionFontBold: 1,
 
-  public radarChartData: any = [
-    {data: [65, 59, 90, 81, 56, 55, 40], label: 'Series A'},
-    {data: [28, 48, 40, 19, 96, 27, 100], label: 'Series B'}
-  ];
-  public radarChartType = 'radar';
+      // Subtitulo
+      subCaption: '',
+      subcaptionFontSize: 32,
+      subcaptionFontColor: '',
 
-  public canvasWidth = 200;
-  public needleValue = 65;
-  public centralLabel = '';
-  public name = 'Calorias';
-  public bottomLabel = '65';
-  public options = {
-    hasNeedle: true,
-    needleColor: 'black',
-    needleUpdateSpeed: 1000,
-    arcColors: ['red', 'green', 'lightgray'],
-    arcDelimiters: [30, 60],
-    rangeLabel: ['0', '100'],
-    needleStartValue: 50,
-  };
+      // Values
+      valueFontSize: 20,
+      majorTMNumber: 2,
+      lowerLimitDisplay: 'Muy Bajo',
+      upperLimitDisplay: 'Demasiado',
+      valueBelowPivot: 1,
+      showTickMarks: 0,
+      showTickValues: 1,
+      placeValuesInside: 1,
+      lowerLimit: 0,
+      upperLimit: 100,
+      pivotRadius: 10,
 
-  dataSource = {
-    'chart': {
-      'caption': 'Nordstorm\'s Customer Satisfaction Score for 2017',
-      'lowerLimit': '0',
-      'upperLimit': '100',
-      'showValue': '1',
-      'numberSuffix': '%',
-      'theme': 'fusion',
-      'showToolTip': '0'
+      decimals: 1,
+      showValue: 1,
+      theme: 'fusion',
+      showToolTip: 1,
     },
-    // Gauge Data
-    'colorRange': {
-      'color': [{
-        'minValue': '0',
-        'maxValue': '50',
-        'code': '#F2726F'
+    // Colores
+    colorRange: {
+      color: [{
+        minValue: 0,
+        maxValue: 20,
+        code: '#e44a00',
       }, {
-        'minValue': '50',
-        'maxValue': '75',
-        'code': '#FFC533'
+        minValue: 20,
+        maxValue: 40,
+        code: '#f8bd19'
       }, {
-        'minValue': '75',
-        'maxValue': '100',
-        'code': '#62B58F'
+        minValue: 40,
+        maxValue: 60,
+        code: '#6baa01',
+        display: 'Hola'
+      }, {
+        minValue: 60,
+        maxValue: 80,
+        code: '#f8bd19'
+      }, {
+        minValue: 80,
+        maxValue: 100,
+        code: '#e44a00'
       }]
     },
-    'dials': {
-      'dial': [{
-        'value': '81'
+    // Datos
+    dials: {
+      dial: [{
+        value: 0
       }]
     }
   };
 
+  public InfoValores = {
+    radarChartLabels:
+      ['Eating', 'Drinking', 'Sleeping', 'Designing', 'Coding', 'Cycling', 'Running'],
+    radarChartData: [{
+      data:
+        [65, 59, 90, 81, 56, 55, 40],
+      label: 'Series A'
+    }, {
+      data:
+        [28, 48, 40, 19, 96, 27, 100],
+      label: 'Series B'
+    }],
+    radarChartType: 'radar'
+  };
 
   constructor(private _EvaluacionService: EvaluacionService,
               public dialog: MatDialog,
               public snackBar: MatSnackBar) {
   }
 
-  ngOnInit() {
-    //const responseSemana = this._EvaluacionService.getEvaluacionSemana().toPromise();
-    //const responseDia = this._EvaluacionService.getEvaluacionDia().toPromise();
-    //console.log(responseSemana);
-    //console.log(responseDia);
+  async ngOnInit() {
+    this.ValoresDia = await this._EvaluacionService.getEvaluacionDia().toPromise();
+    this.ValoresSemana = await this._EvaluacionService.getEvaluacionSemana().toPromise();
+    this.cargaDatos = true;
+
+    console.log(this.ValoresDia);
+    if (this.mostrarSemana) {
+      this.graficoCalorias(this.ValoresSemana);
+    } else {
+      this.graficoCalorias(this.ValoresDia);
+    }
+  }
+
+  async graficoCalorias(Periodo) {
+    // Definiciones
+    let i = 0, ubicacion = 0;
+    const porcMin = [0, 0.1, 0.3, 0.7, 0.9];
+    const porcMax = [0.1, 0.3, 0.7, 0.9, 1];
+    const colores = ['#e44a00', '#f8bd19', '#6baa01', '#f8bd19', '#e44a00'];
+    const subCaption = ['MUY BAJO', 'BAJO', 'BIEN', 'ALTO', 'DEMASIADO'];
+    this.ValoresSelected = Periodo;
+
+    console.log(Periodo);
+    const calorias = Periodo.Valores.find(x => x.ValorNutricional.Nombre === 'Calorias');
+
+    // Calorias requeridas SIN COLACION (CAMBIAR)
+    const CaloriasRequerida = Math.round((calorias.CantidadRequerida * 100) / 100) * 0.86;
+    this.CaloriasRequerida = CaloriasRequerida;
+    this.CaloriasRecomendada = calorias.CantidadConsumida;
+
+    this.InfoCalorias.chart.upperLimit = CaloriasRequerida * 2;
+    this.InfoCalorias.colorRange.color.forEach(function (element) {
+      element.minValue = ((CaloriasRequerida * 2) * porcMin[i]);
+      element.maxValue = ((CaloriasRequerida * 2) * porcMax[i]);
+      if (element.minValue < calorias.CantidadConsumida && element.maxValue > calorias.CantidadConsumida) {
+        ubicacion = i;
+      }
+      i++;
+    });
+    this.InfoCalorias.dials.dial.forEach(x => x.value = calorias.CantidadConsumida);
+    this.InfoCalorias.chart.subCaption = subCaption[ubicacion];
+    this.InfoCalorias.chart.subcaptionFontColor = colores[ubicacion];
+
+  }
+
+  async graficoValores() {
+
   }
 
   openSnackBar(message: string, action: string) {
@@ -172,7 +241,7 @@ export class EvaluacionConfigComponent {
   Selected = {
     dia: null,
     config: null
-  }
+  };
   defaultConfig = [
     {value: true, titulo: 'Semana'},
     {value: false, titulo: 'Dia'}];
