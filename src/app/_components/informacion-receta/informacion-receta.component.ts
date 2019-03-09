9c97c9ea76d96e24a09f5fce5acafadb73878922
leancoggiola <leandrocoggiola@hotmail.com>
2019-03-09 17:43:46 -0300

    9/3/19 Favorito boton implementado

--- src/app/_components/informacion-receta/informacion-receta.component.ts
+++ src/app/_components/informacion-receta/informacion-receta.component.ts
@@ -1,12 +1,14 @@
 import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 import {Receta} from '../../_models/Receta';
 import {RecetaService} from '../../_services/receta.service';
+import {FavoritosService} from '../../_services/favoritos.service';
+import {MatSnackBar} from '@angular/material';
 
 @Component({
   selector: 'app-informacion-receta',
   templateUrl: './informacion-receta.component.html',
   styleUrls: ['./informacion-receta.component.css'],
-  providers: [RecetaService]
+  providers: [RecetaService, FavoritosService]
 })
 export class InformacionRecetaComponent implements OnInit {
   @Input() RecetaElegida: Receta;
@@ -14,13 +16,17 @@ export class InformacionRecetaComponent implements OnInit {
   DatosReceta;
   recetaCargandoInfo = true;
 
-  constructor(public _RecetaService: RecetaService) { }
+  constructor(private _RecetaService: RecetaService,
+              private _FavoritoService: FavoritosService,
+              public snackBar: MatSnackBar) {
+  }
 
   ngOnInit() {
     this._RecetaService.verInformacionReceta(this.RecetaElegida).subscribe(response => {
+      console.log(response);
       this.DatosReceta = response;
       this.RecetaElegida = this.DatosReceta;
-      this.RecetaElegida.Nombre = this.RecetaElegida.Nombre[0].toUpperCase() +  this.RecetaElegida.Nombre.substr(1).toLowerCase();
+      this.RecetaElegida.Nombre = this.RecetaElegida.Nombre[0].toUpperCase() + this.RecetaElegida.Nombre.substr(1).toLowerCase();
       this.RecetaElegida.Ingredientes.forEach(function (element) {
         element.Ingrediente.Nombre = element.Ingrediente.Nombre[0].toUpperCase() + element.Ingrediente.Nombre.substr(1).toLowerCase();
       });
@@ -30,6 +36,28 @@ export class InformacionRecetaComponent implements OnInit {
 
   volver() {
     this.volverEvent.emit(false);
+  }
+
+  openSnackBar(message: string, action: string) {
+    this.snackBar.open(message, action, {
+      duration: 2000,
+    });
+  }
+
+  togglefavorito() {
+    this._FavoritoService.HacerFavortito(this.RecetaElegida).subscribe(res => {
+      console.log(res);
+      this.RecetaElegida.Favorito = !this.RecetaElegida.Favorito;
+      let mensaje = '';
+      if (this.RecetaElegida.Favorito === true) {
+        mensaje = 'Agregado a Favoritas';
+      } else {
+        mensaje = 'Eliminado de Favoritas';
+      }
+
+      this.openSnackBar(mensaje, 'Descartar');
+
+    });
   }
 
 }
--- src/app/_views/favoritos-view/favoritos-view.component.ts
+++ src/app/_views/favoritos-view/favoritos-view.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import {FavoritosService} from '../../_services/favoritos.service';
 
 @Component({
   selector: 'app-favoritos-view',
   templateUrl: './favoritos-view.component.html',
-  styleUrls: ['./favoritos-view.component.css']
+  styleUrls: ['./favoritos-view.component.css'],
+  providers: [FavoritosService]
 })
 export class FavoritosViewComponent implements OnInit {
 
-  constructor() { }
+  cargardatos = false;
+  listFavoritos = null;
+
+  constructor(private _FavoritosService: FavoritosService) { }
 
   ngOnInit() {
+    this._FavoritosService.RecetasxUsuario().subscribe(res => {
+      console.log(res);
+      this.listFavoritos = res;
+    });
   }
 
 }
